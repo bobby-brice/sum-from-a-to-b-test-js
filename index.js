ce498d6d0e6d761fb1fa3d5de8700010aed1dcad
bobby-brice <bobby.brice@gmail.com>
2020-05-30 17:51:51 +0000

    sum from a to b using recursion

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 
+// eslint-disable-next-line func-style
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+
+  if (fromN === 0) {
+    return toN;
+  } else if (fromN === toN) {
+    return fromN;
+  } else if (fromN === (toN - 1)) {
+    return fromN + toN;
+  }
+  if ((toN - 1) > fromN) {
+    return fromN + sum(fromN + 1, toN);
+  }
+
 }
+console.log(sum(3,7));
 
 module.exports = sum;
